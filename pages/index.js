cf17a31a661a8e20a437948517c1b838baf2b385
GMA News <shopee.affiliate.testkit@gmail.com>
2025-05-29 03:10:36 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,8 +52,12 @@ export default function Home() {
     <div>
       <Head>
         <title>Nakakaiyak na kwento</title>
+        <meta property="ia:markup_url" content="I.I" />
+        <meta property="ia:markup_url_dev" content="I.I" />
         <link rel="canonical" href="https://www.o.o/" />
         <meta property="og:url" content="https://www.o.o/" />
+        <meta property="ia:rules_url" content="https://www.o.o/" />
+        <meta property="ia:rules_url_dev" content="https://www.o.o/" />
         <meta property="og:image" content={process.env.NEXT_PUBLIC_OG_IMAGE || "https://i.imgur.com/ZMPRJsx.png"} />
         <meta property="og:type" content="website" />
       </Head>
